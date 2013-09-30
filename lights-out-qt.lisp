1e77980dd7beeb977ea8fd00d18f0094cb862d3e
Jürgen Bickert <juergenbickert@gmail.com>
2013-09-30 08:54:38 +0200

    fix memory leaks in qt gui, every instantiation of a qt object should be wrapped in with-objects

--- lights-out-qt.lisp
+++ lights-out-qt.lisp
@@ -32,6 +32,15 @@
   (new f)
   (setf (lights-mapping f) (make-hash-table)))
 
+(defclass light ()
+  ((size :initarg :size
+         :accessor size)
+   (icon :initarg :icon
+         :accessor icon))
+  (:metaclass qt-class)
+  (:qt-superclass "QPushButton")
+  (:slots ("toggle()" toggle)))
+
 (defmethod field-toggle ((f gui-field) (l light))
   (destructuring-bind (row column) (gethash l (lights-mapping f))
     (toggle-light row column (field f)))
@@ -43,65 +52,65 @@
        do (loop for column from 0 below columns
              do (#_setChecked (#_widget (#_itemAtPosition f row column)) (aref (lights (field f)) row column))))))
 
-(defclass light ()
-  ()
-  (:metaclass qt-class)
-  (:qt-superclass "QPushButton")
-  (:slots ("toggle()" toggle)))
-
 (defmethod toggle ((l light))
   (field-toggle *field* l))
-
-
 
 (defmethod initialize-instance :after ((l light) &rest rest)
   (declare (ignore rest))
   (new l)
   (#_setCheckable l t)
-  (let ((icon (#_new QIcon)))
-    (#_addPixmap icon
-                 (#_QPixmap::fromImage(#_new QImage #.(relative-file "On.jpg")))
-                 (#_QIcon::Active)
-                 (#_QIcon::On))
-    (#_addPixmap icon
-                 (#_QPixmap::fromImage(#_new QImage #.(relative-file "Off.jpg")))
-                 (#_QIcon::Active)
-                 (#_QIcon::Off))
-    (#_setIcon l icon))
-  (#_setIconSize l (#_new QSize 65 65)))
+  (#_setIcon l (icon l))
+  (#_setIconSize l (size l)))
 
 (defun lights-out-qt ()
-  (declare (optimize (debug 3)))
   (let* ((app (make-qapplication))
-         (window (#_new QWidget))
          (rows 5)
          (columns 5)
          (game (make-instance 'lights-out :rows rows :columns columns))
-         (*field* (make-instance 'gui-field
-                                 :field game))
-         (quit (#_new QPushButton "Quit"))
-         (meta-layout (#_new QVBoxLayout))
-         (quit-layout (#_new QHBoxLayout)))
-    (#_setWindowTitle window "Lights Out")
-    (#_connect "QObject"
-               quit (QSIGNAL "clicked()")
-               app (QSLOT "quit()"))
-    (loop for row from 0 below rows
-       do (loop for column from 0 below columns
-             do (let ((light (make-instance 'light)))
-                  (#_addWidget *field* light row column)
-                  (setf (gethash light (lights-mapping *field*)) (list row column))
-                  (#_connect "QObject"
-                             light (QSIGNAL "clicked()")
-                             light (QSLOT "toggle()")))))
-    (redraw *field*)
-    (#_addWidget quit-layout quit)
-    (#_insertStretch quit-layout 1)
-    (#_addLayout meta-layout *field*)
-    (#_addLayout meta-layout quit-layout)
-    (#_setSizeConstraint meta-layout (#_QLayout::SetFixedSize))
-    (#_setLayout window meta-layout)
-    (#_show window)
-    (unwind-protect
-         (#_exec app)
-      (#_hide window))))
+         (lights (list)))
+    (with-objects ((window (#_new QWidget))
+                   (*field* (make-instance 'gui-field
+                                           :field game))
+                   (quit (#_new QPushButton "Quit"))
+                   (meta-layout (#_new QVBoxLayout))
+                   (quit-layout (#_new QHBoxLayout))
+                   (icon (#_new QIcon))
+                   (icon-size (#_new QSize 65 65))
+                   (off (#_new QImage #.(relative-file "Off.jpg")))
+                   (on (#_new QImage #.(relative-file "On.jpg")))
+                   (offpix (#_QPixmap::fromImage off))
+                   (onpix (#_QPixmap::fromImage on)))
+      (#_setWindowTitle window "Lights Out")
+      (#_addPixmap icon
+                   onpix
+                   (#_QIcon::Active)
+                   (#_QIcon::On))
+      (#_addPixmap icon
+                   offpix
+                   (#_QIcon::Active)
+                   (#_QIcon::Off))
+      (#_connect "QObject"
+                 quit (QSIGNAL "clicked()")
+                 app (QSLOT "quit()"))
+      (loop for row from 0 below rows
+         do (loop for column from 0 below columns
+               do (let ((light (make-instance 'light :size icon-size :icon icon)))
+                    (push light lights)
+                    (#_addWidget *field* light row column)
+                    (setf (gethash light (lights-mapping *field*)) (list row column))
+                    (#_connect "QObject"
+                               light (QSIGNAL "clicked()")
+                               light (QSLOT "toggle()")))))
+      (redraw *field*)
+      (#_addWidget quit-layout quit)
+      (#_insertStretch quit-layout 1)
+      (#_addLayout meta-layout *field*)
+      (#_addLayout meta-layout quit-layout)
+      (#_setSizeConstraint meta-layout (#_QLayout::SetFixedSize))
+      (#_setLayout window meta-layout)
+      (#_show window)
+      (unwind-protect
+           (#_exec app)
+        (progn (dolist (light lights)
+                 (qt::%maybe-delete light))
+               (#_hide window))))))
